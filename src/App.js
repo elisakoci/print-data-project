2d1d2119f1ff57087dc8e4d976d298cfe6136890
Elisa Omeri <elisa.omeri007@gmail.com>
2025-03-27 01:50:54 +0100

    project - structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Layout} from "antd";
+import Header from "./components/Header";
+import './App.css'; 
+
+const App = () => {
+
+  return (
+    <Layout className="App">
+      <Header />
+    </Layout>
+  );
+};
+
+export default App;
+
